2da9dd191a037e437d6ed5071aa4ba69d9219aff
Renan Rodrigues <irenan.rodriguees@gmail.com>
2023-09-24 20:30:32 -0300

    exercicio cev-javascript 10

---
+++ curso-em-video/cev-javascript/exercicios/ex10-acoes-variaveis-dinamismo/acoes.js
@@ -0,0 +1,17 @@
+let res = document.getElementById('saida')
+
+function acao1() {
+  res.innerHTML += `<p>Clicou no <strong>1º botão</strong>.</p>`
+}
+
+function acao2() {
+  res.innerHTML += `<p>Clicou no <strong>2º botão</strong>.</p>`
+}
+
+function acao3() {
+  res.innerHTML += `<p>Clicou no <strong>3º botão</strong>.</p>`
+}
+
+function acao4() {
+  res.innerHTML += `<p>Clicou no <strong>4º botão</strong>.</p>`
+}
